5c51050dbab35aac58a6a90f8f8931b81bbcd6c7
Ganeshaprasad1996 <prasadgk107@gmail.com>
2023-08-19 18:04:58 +0530

    tic-toc-toe-js application

---
+++ script.js
@@ -0,0 +1,21 @@
+let music =new Audio("music.mp3")
+let audioTurn =new Audio("ting.mp3")
+let gameOver =new Audio("gameover.mp3")
+
+let turn="X";
+
+//Function to change turn
+
+const changeTurn =()=>
+{
+    return turn === "X"?"O":"X"
+}
+
+//Function to check win
+
+const checkWin=()=>
+{
+
+}
+
+//Game logic
